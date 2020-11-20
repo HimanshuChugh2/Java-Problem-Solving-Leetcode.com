51d1a2ea71e35261c6b4fdb0611adff1ceacf85b
HimanshuChugh2 <39953898+HimanshuChugh2@users.noreply.github.com>
2020-11-20 22:02:02 +0530

    0 ms

---
+++ solutions/198. House Robber.java
@@ -0,0 +1,14 @@
+class Solution {
+   public static int rob(int[] nums) {
+​
+        if (nums.length==0)return 0;
+        int[] memo=new int[nums.length+1];
+        memo[0]=0;
+        memo[1]=nums[0];
+        for (int i=1;i<nums.length;i++)
+        {
+            memo[i+1]=Math.max(memo[i-1]+nums[i],memo[i]);
+        }
+        return memo[nums.length];
+    }
+}

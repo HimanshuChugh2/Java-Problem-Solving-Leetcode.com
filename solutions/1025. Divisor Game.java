05cff3734ce512cfd8c15e1edaff88a8fbdb8a3a
HimanshuChugh2 <39953898+HimanshuChugh2@users.noreply.github.com>
2020-11-20 22:18:22 +0530

    0 ms

---
+++ solutions/1025. Divisor Game.java
@@ -0,0 +1,14 @@
+class Solution {
+ 
+   
+    
+    public  boolean divisorGame(int N) {
+    if (N%2==0)
+       {
+           return true;
+       }
+       else return  false;
+        
+       
+    }
+}

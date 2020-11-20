7633d390b4717f441b4e72ae964d99f61fdcb1b4
HimanshuChugh2 <39953898+HimanshuChugh2@users.noreply.github.com>
2020-11-20 22:01:14 +0530

    N/A

--- solutions/169. Majority Element.java
+++ solutions/169. Majority Element.java
@@ -1,33 +1,35 @@
 class Solution {
     
     public int majorityElement(int[] nums) 
-    {  
-        int len=nums.length/2;
+    {
+        int maxSoFar=0;
         int maxCurr=0;
         int majEle=0;
        
         for (int i=0;i<nums.length;i++)
-        {    
-                 maxCurr=0;
-                 for (int j=0;j<nums.length;j++) {
-                    if (i == j ) {
-                    } else {
-                        if (nums[i] == nums[j]) {
-                            maxCurr++;
-                            if(maxCurr>=len)
-                            {
-                                 return nums[i];}
-                        }
+        {
+             
+            if (maxSoFar<maxCurr) {
+                maxSoFar = maxCurr;
+                majEle=i-1;
+                if(maxSoFar>=(nums.length/2))
+             //       System.out.println("returning majEle");
+                {return nums[majEle];}
+           
+            }
+            maxCurr=0;
+​
+            for (int j=0;j<nums.length;j++) {
+                if (i == j ) {
+                } else {
+                    if (nums[i] == nums[j]) {
+                        maxCurr++;
                     }
                 }
+            }
         }
-    return nums[majEle];
-    }
-    }
      
-​
- 
+return nums[majEle];
         
-        
-    
-​
+    }    
+    }
